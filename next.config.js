1112c41519e43264a9f7a4e1adc097af8c0ec52c
dickpic <hurstay4@gmail.com>
2025-07-14 18:27:42 +0300

    оптимизация

--- next.config.js
+++ next.config.js
@@ -1,17 +1,38 @@
 /** @type {import('next').NextConfig} */
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-	enabled: false,
+  enabled: false,
 });
 
 module.exports = withBundleAnalyzer({
-	webpack: (config, options) => {
-		config.module.rules.push({
-			test: /\.svg$/,
-			issuer: /\.[jt]sx?$/,
-			use: ['@svgr/webpack'],
-		})
+  images: {
+    formats: ['image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
 
-		return config;
-	}
+    config.module.rules.push({
+      test: /\.(jpe?g|png)$/i,
+      use: [
+        {
+          loader: 'next-image-loader',
+          options: {
+            quality: 80,
+            format: 'webp',
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+  productionBrowserSourceMaps: false,
+  optimizeFonts: true,
+  compress: true,
 });
