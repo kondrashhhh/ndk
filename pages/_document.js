7d769fff9707146731b4a56449e8c33e70ae8101
dickpic <hurstay4@gmail.com>
2025-08-13 23:11:03 +0300

    сео правки

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,12 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
+                <title>НОВОСЕЛЬЕ девелопмент. Квартиры в НОВОСЕЛЬЕ</title>
+				<meta name="description" content="Квартиры в проекте ЖК Уютный: городские кварталы от застройщика НОВОСЕЛЬЕ девелопмент. Дома комфорт-класса в районе Новоселье. План квартир, площадь, количество комнат можно увидеть на сайте. Остались вопросы? Звоните +7 812 309-77-77"></meta>
+				<link rel="canonical" href="https://ddq.ru/" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Prata&display=swap" rel="stylesheet"></link>
                 <link rel="icon" type="image/png" href="/favicon.png"></link>
 			</Head>
 			<body>
--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,6 @@ const Page = (props) => {
 	return (
 		<>
 			<Head>
-				<title>НОВОСЕЛЬЕ девелопмент. Квартиры в НОВОСЕЛЬЕ</title>
-				<meta name="description" content="Квартиры в проекте ЖК Уютный: городские кварталы от застройщика НОВОСЕЛЬЕ девелопмент. Дома комфорт-класса в районе Новоселье. План квартир, площадь, количество комнат можно увидеть на сайте. Остались вопросы? Звоните +7 812 309-77-77"></meta>
-				<link rel="canonical" href="https://ddq.ru/" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Prata&display=swap" rel="stylesheet"></link>
 			</Head>
 			<Home {...props} />
 		</>
